45b73f52e1c7d6f01e07b499e942a81df13bb709
kitUIN <kulujun@gmail.com>
2023-03-15 16:38:51 +0800

    修改收发包

--- src/main/java/github/kituin/chatimage/client/ChatImageClient.java
+++ src/main/java/github/kituin/chatimage/client/ChatImageClient.java
@@ -7,6 +7,7 @@ import github.kituin.chatimage.command.ChatImageCommand;
 import github.kituin.chatimage.config.ChatImageConfig;
 import github.kituin.chatimage.gui.ConfigScreen;
 import github.kituin.chatimage.tool.ChatImageFrame;
+import github.kituin.chatimage.tool.ChatImagePacket;
 import github.kituin.chatimage.tool.ChatImageUrl;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -16,7 +17,6 @@ import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.texture.NativeImage;
@@ -30,12 +30,14 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
 import static com.mojang.brigadier.arguments.StringArgumentType.string;
 import static github.kituin.chatimage.ChatImage.*;
 import static github.kituin.chatimage.tool.ChatImageCode.CACHE_MAP;
+import static github.kituin.chatimage.tool.ChatImagePacket.*;
 
 /**
  * @author kitUIN
@@ -62,9 +64,12 @@ public class ChatImageClient implements ClientModInitializer {
         };
         ChatImageUrl.networkHelper = (url, file, isServer) -> {
             if (isServer) {
-                sendFilePackets(MinecraftClient.getInstance().player, url, file, FILE_CANNEL);
+                List<PacketByteBuf> bufs = createFilePacket(url, file);
+                if (bufs != null) {
+                    sendPacketAsync(MinecraftClient.getInstance().player, FILE_CHANNEL, bufs);
+                }
             } else {
-                tryGetFromServer(url);
+                loadFromServer(url);
             }
         };
         configKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
@@ -78,79 +83,34 @@ public class ChatImageClient implements ClientModInitializer {
                 client.setScreen(new ConfigScreen(null));
             }
         });
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-            dispatcher.register(
-                    LiteralArgumentBuilder.<FabricClientCommandSource>literal("chatimage").executes(ChatImageCommand::help)
-                            .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("send")
-                                    .then(RequiredArgumentBuilder.<FabricClientCommandSource, String>argument("name", string())
-                                            .then(RequiredArgumentBuilder.<FabricClientCommandSource, String>argument("url", greedyString())
-                                                    .executes(ChatImageCommand::sendChatImage)
-                                            )
-                                    )
-                            )
-                            .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("url")
-                                    .then(RequiredArgumentBuilder.<FabricClientCommandSource, String>argument("url", greedyString())
-                                            .executes(ChatImageCommand::sendChatImage)
-                                    )
-                            )
-                            .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("help")
-                                    .executes(ChatImageCommand::help)
-                            )
-                            .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("reload")
-                                    .executes(ChatImageCommand::reloadConfig)
-                            )
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(
+                LiteralArgumentBuilder.<FabricClientCommandSource>literal("chatimage").executes(ChatImageCommand::help)
+                        .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("send")
+                                .then(RequiredArgumentBuilder.<FabricClientCommandSource, String>argument("name", string())
+                                        .then(RequiredArgumentBuilder.<FabricClientCommandSource, String>argument("url", greedyString())
+                                                .executes(ChatImageCommand::sendChatImage)
+                                        )
+                                )
+                        )
+                        .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("url")
+                                .then(RequiredArgumentBuilder.<FabricClientCommandSource, String>argument("url", greedyString())
+                                        .executes(ChatImageCommand::sendChatImage)
+                                )
+                        )
+                        .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("help")
+                                .executes(ChatImageCommand::help)
+                        )
+                        .then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("reload")
+                                .executes(ChatImageCommand::reloadConfig)
+                        )
 
-            );
+        ));
+        ClientPlayNetworking.registerGlobalReceiver(DOWNLOAD_FILE_CHANNEL, (client, handler, buf, responseSender) -> {
+            ChatImagePacket.clientDownloadFileChannelReceived(buf);
         });
-        ClientPlayNetworking.registerGlobalReceiver(DOWNLOAD_FILE_CANNEL, (client, handler, buf, responseSender) -> {
-            for (Map.Entry<String, byte[]> entry : buf.readMap(PacketByteBuf::readString, PacketByteBuf::readByteArray).entrySet()) {
-                String[] order = entry.getKey().split("->");
-                if (order[1].equals(order[0]) && "0".equals(order[1])) {
-                    CACHE_MAP.put(order[2], new ChatImageFrame<>(ChatImageFrame.FrameError.FILE_NOT_FOUND));
-                    return;
-                }
-                HashMap<Integer, byte[]> list = new HashMap<>();
-                if (CLIENT_CACHE_MAP.containsKey(order[2])) {
-                    list = CLIENT_CACHE_MAP.get(order[2]);
-                }
-                list.put(Integer.valueOf(order[0]), entry.getValue());
-                CLIENT_CACHE_MAP.put(order[2], list);
-                if (list.size() == Integer.parseInt(order[1])) {
-                    LOGGER.info("[merge]" + order[2]);
-                    int length = 0;
-                    for (Map.Entry<Integer, byte[]> en : list.entrySet()) {
-                        length += en.getValue().length;
-                    }
-                    ByteBuffer bb = ByteBuffer.allocate(length);
-                    for (int i = 0; i < list.size(); i++) {
-                        bb.put(list.get(i));
-                    }
-                    try {
-                        ChatImageUrl.putLocalFile(new ByteArrayInputStream(bb.array()), order[2]);
-                    } catch (IOException e) {
-                        LOGGER.error(e.toString());
-                        CACHE_MAP.put(order[2], new ChatImageFrame(ChatImageFrame.FrameError.SERVER_FILE_LOAD_ERROR));
-                    }
-                }
-            }
-
+        ClientPlayNetworking.registerGlobalReceiver(GET_FILE_CHANNEL, (client, handler, buf, responseSender) -> {
+            ChatImagePacket.clientGetFileChannelReceived(buf);
         });
-
     }
 
-    /**
-     * 尝试从服务器获取图片
-     *
-     * @param url 图片url
-     */
-    public static void tryGetFromServer(String url) {
-        if (MinecraftClient.getInstance().player != null) {
-            PacketByteBuf buf = PacketByteBufs.create();
-            buf.writeString(url);
-            sendFilePacketAsync(MinecraftClient.getInstance().player, GET_FILE_CANNEL, buf);
-            LogUtils.getLogger().info("[try get from server]" + url);
-        } else {
-            CACHE_MAP.put(url, new ChatImageFrame<>(ChatImageFrame.FrameError.FILE_NOT_FOUND));
-        }
-    }
 }
