ba13162b70ea8984b2560a4ef8c4b5076d83fbab
kitUIN <kulujun@gmail.com>
2023-03-15 18:50:05 +0800

    1.1.4

--- src/main/java/github/kituin/chatimage/mixin/ScreenMixin.java
+++ src/main/java/github/kituin/chatimage/mixin/ScreenMixin.java
@@ -69,8 +69,8 @@ public abstract class ScreenMixin extends AbstractParentElement implements Drawa
                         int j = viewHeight + CONFIG.paddingTop + CONFIG.paddingBottom;
                         int l = x + 12;
                         int m = y - 12;
-                        if (l + i > this.width) {
-                            l = this.width - i;
+                        if (l + i + 3 > this.width) {
+                            l = this.width - i - 3;
                         }
                         if (m + j + 6 > this.height) {
                             m = this.height - j - 6;
