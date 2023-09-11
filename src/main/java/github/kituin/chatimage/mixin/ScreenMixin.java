da1063b46c38b2e550c398c4f98381d398c58126
Nacho <kulujun@gmail.com>
2023-09-11 23:49:27 +0800

    update

--- src/main/java/github/kituin/chatimage/mixin/ScreenMixin.java
+++ src/main/java/github/kituin/chatimage/mixin/ScreenMixin.java
@@ -1,9 +1,10 @@
 package github.kituin.chatimage.mixin;
 
+import com.github.chatimagecode.ChatImageCode;
+import com.github.chatimagecode.ChatImageFrame;
 import com.mojang.blaze3d.systems.RenderSystem;
 import github.kituin.chatimage.gui.ConfirmNsfwScreen;
-import github.kituin.chatimage.tool.ChatImageCode;
-import github.kituin.chatimage.tool.ChatImageFrame;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.AbstractParentElement;
 import net.minecraft.client.gui.Drawable;
@@ -27,9 +28,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
+import static com.github.chatimagecode.ChatImageCode.NSFW_MAP;
+import static com.github.chatimagecode.ChatImageHandler.AddChatImage;
 import static github.kituin.chatimage.client.ChatImageClient.CONFIG;
-import static github.kituin.chatimage.tool.ChatImageCode.NSFW_MAP;
-import static github.kituin.chatimage.tool.ChatImageHandler.AddChatImage;
 import static github.kituin.chatimage.tool.ChatImageStyle.SHOW_IMAGE;
 
 /**
