3a7f12ee5265723923f1de6e36a97e851da039fd
Nacho <kulujun@gmail.com>
2024-02-04 02:43:18 +0800

    1.2.0

--- src/main/java/github/kituin/chatimage/gui/ConfigScreen.java
+++ src/main/java/github/kituin/chatimage/gui/ConfigScreen.java
@@ -1,8 +1,8 @@
 package github.kituin.chatimage.gui;
 
-import github.kituin.chatimage.config.ChatImageConfig;
 import github.kituin.chatimage.widget.GifSlider;
 import github.kituin.chatimage.widget.TimeOutSlider;
+import io.github.kituin.ChatImageCode.ChatImageConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.Screen;
@@ -11,6 +11,7 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
 import static github.kituin.chatimage.client.ChatImageClient.CONFIG;
+import static net.minecraft.screen.ScreenTexts.composeGenericOptionText;
 
 @Environment(EnvType.CLIENT)
 public class ConfigScreen extends ConfigRawScreen {
@@ -39,17 +40,37 @@ public class ConfigScreen extends ConfigRawScreen {
             button.setMessage(getCq(CONFIG.cqCode));
             ChatImageConfig.saveConfig(CONFIG);
         }, getButtonTooltip(Text.translatable("cq.chatimage.tooltip"))));
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 77, this.height / 4 + 96 + -16, 150, 20, Text.translatable("gui.back"), (button) -> {
+        this.addDrawableChild(new ButtonWidget(this.width / 2 + 4, this.height / 4 + 72 - 16, 150, 20, getDrag(CONFIG.dragUseCicode), (button) -> {
+            CONFIG.dragUseCicode = !CONFIG.dragUseCicode;
+            button.setMessage(getDrag(CONFIG.dragUseCicode));
+            ChatImageConfig.saveConfig(CONFIG);
+        }, getButtonTooltip(Text.translatable("drag.chatimage.tooltip"))));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 154, this.height / 4 + 96 - 16, 150, 20, getUri(CONFIG.checkImageUri), (button) -> {
+            CONFIG.checkImageUri = !CONFIG.checkImageUri;
+            button.setMessage(getUri(CONFIG.checkImageUri));
+            ChatImageConfig.saveConfig(CONFIG);
+        }));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 77, this.height / 4 + 120 + -16, 150, 20, Text.translatable("gui.back"), (button) -> {
             if (this.client != null) {
                 this.client.setScreen(this.parent);
             }
         }));
     }
 
-    private MutableText getNsfw(boolean enable) {
-        return Text.translatable(enable ? "close.nsfw.chatimage.gui" : "open.nsfw.chatimage.gui");
-    }
     private MutableText getCq(boolean enable) {
-        return Text.translatable(enable ? "open.cq.chatimage.gui" : "close.cq.chatimage.gui");
+        return getEnable( "cq.chatimage.gui", enable);
+    }
+    private MutableText getNsfw(boolean enable) {
+        return getEnable( "nsfw.chatimage.gui", !enable);
+    }
+    private MutableText getDrag(boolean enable) {
+        return getEnable("drag.chatimage.gui", enable);
+    }
+    private MutableText getUri(boolean enable) {
+        return getEnable("uri.chatimage.gui", enable);
+    }
+    public static MutableText getEnable(String key,boolean enable)
+    {
+        return composeGenericOptionText(Text.translatable(key),Text.translatable((enable ? "open" : "close") + ".chatimage.common"));
     }
 }
