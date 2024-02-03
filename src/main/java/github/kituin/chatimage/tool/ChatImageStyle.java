d5aa877a92adcf4bcd7db006e8fc50ddf95d4e44
Nacho <kulujun@gmail.com>
2024-02-03 20:16:53 +0800

    1.2.0

--- src/main/java/github/kituin/chatimage/tool/ChatImageStyle.java
+++ src/main/java/github/kituin/chatimage/tool/ChatImageStyle.java
@@ -1,23 +1,24 @@
 package github.kituin.chatimage.tool;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import io.github.kituin.ChatImageCode.ChatImageCode;
-import io.github.kituin.ChatImageCode.exception.InvalidChatImageUrlException;
+import io.github.kituin.ChatImageCode.ChatImageCodeInstance;
+import io.github.kituin.ChatImageCode.exception.InvalidChatImageCodeException;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.JsonHelper;
 
 
 /**
  * @author kitUIN
  */
 public class ChatImageStyle {
-    public static final HoverEvent.Action<ChatImageCode> SHOW_IMAGE = new HoverEvent.Action<>("show_chatimage", true, ChatImageStyle::fromJson, ChatImageStyle::toJson, ChatImageStyle::fromJson);
-
+    public static final HoverEvent.Action<ChatImageCode> SHOW_IMAGE =
+            new HoverEvent.Action<>("show_chatimage", true,
+                    ChatImageCode::fromJson,
+                    ChatImageCode::toJson,
+                    ChatImageStyle::fromJson);
     /**
      * 文本 悬浮图片样式
      *
@@ -25,9 +26,20 @@ public class ChatImageStyle {
      * @return 悬浮图片样式
      */
     public static Style getStyleFromCode(ChatImageCode code) {
-        return Style.EMPTY.withHoverEvent(new HoverEvent(SHOW_IMAGE, code)).withColor(Formatting.GREEN);
+        return getStyleFromCode(code, Formatting.GREEN);
     }
 
+    /**
+     * 文本 悬浮图片样式
+     *
+     * @param code {@link ChatImageCode}
+     * @param color 颜色
+     * @return 悬浮图片样式
+     */
+    public static Style getStyleFromCode(ChatImageCode code, Formatting color) {
+        Style style = Style.EMPTY.withHoverEvent(new HoverEvent(SHOW_IMAGE, code));
+        return style.withColor(color);
+    }
     /**
      * 获取悬浮图片样式的Text消息
      *
@@ -35,39 +47,21 @@ public class ChatImageStyle {
      * @return {@link MutableText}
      */
     public static MutableText messageFromCode(ChatImageCode code) {
-        MutableText t = (MutableText) Text.of("[");
-        if ("codename.chatimage.default".equals(code.getName())) {
-            t.append(Text.translatable(code.getName()));
-        } else {
-            t.append(Text.of(code.getName()));
-        }
-        t.append("]");
+        MutableText t = code.messageFromCode(
+                Text::literal,
+                Text::translatable,
+                MutableText::append);
         Style style = ChatImageStyle.getStyleFromCode(code);
         return t.fillStyle(style);
     }
 
 
-    public static ChatImageCode fromJson(JsonElement json) {
-        JsonObject jsonObject = json.getAsJsonObject();
-        String code = JsonHelper.getString(jsonObject, "code");
-        try {
-            return new ChatImageCode(code);
-        } catch (InvalidChatImageUrlException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static JsonElement toJson(ChatImageCode code) {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("code", code.toString());
-        return jsonObject;
-    }
 
     public static ChatImageCode fromJson(Text text) {
         try {
-            return new ChatImageCode(text.toString());
-        } catch (InvalidChatImageUrlException e) {
-            throw new RuntimeException(e);
+            return ChatImageCodeInstance.createBuilder().fromCode(text.toString()).build();
+        } catch (InvalidChatImageCodeException e) {
+            return ChatImageCodeInstance.createBuilder().build();
         }
     }
 }
