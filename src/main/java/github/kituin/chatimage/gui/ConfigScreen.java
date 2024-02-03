6da88fdaf4e4903ff8b9efc1f57805c224a33e35
Nacho <kulujun@gmail.com>
2024-02-04 02:40:09 +0800

    1.2.0

--- src/main/java/github/kituin/chatimage/gui/ConfigScreen.java
+++ src/main/java/github/kituin/chatimage/gui/ConfigScreen.java
@@ -62,7 +62,7 @@ public class ConfigScreen extends ConfigRawScreen {
         return getEnable( "cq.chatimage.gui", enable);
     }
     private MutableText getNsfw(boolean enable) {
-        return getEnable( "nsfw.chatimage.gui", enable);
+        return getEnable( "nsfw.chatimage.gui", !enable);
     }
     private MutableText getDrag(boolean enable) {
         return getEnable("drag.chatimage.gui", enable);
