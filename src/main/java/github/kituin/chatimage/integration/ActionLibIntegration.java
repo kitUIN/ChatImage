fc42941804bd31061dcf5d777535991616b23b31
Nacho <kulujun@gmail.com>
2024-02-22 16:18:39 +0800

    update

--- src/main/java/github/kituin/chatimage/integration/ActionLibIntegration.java
+++ src/main/java/github/kituin/chatimage/integration/ActionLibIntegration.java
@@ -1,13 +1,13 @@
 package github.kituin.chatimage.integration;
 
-import io.github.kituin.actionlib.ActionRegisterApi;
+import io.github.kituin.actionlib.IActionRegisterApi;
 import net.minecraft.text.HoverEvent;
 
 import java.util.List;
 
 import static github.kituin.chatimage.tool.ChatImageStyle.SHOW_IMAGE;
 
-public class ActionLibIntegration implements ActionRegisterApi {
+public class ActionLibIntegration implements IActionRegisterApi {
     @Override
     public List<HoverEvent.Action> registerHoverEventAction() {
         return List.of(SHOW_IMAGE);
