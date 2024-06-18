e53694ffeb0327862750b9249cda35ddcd7b041e
Nacho <kulujun@gmail.com>
2024-06-18 15:11:06 +0800

    update

--- src/main/java/io/github/kituin/chatimage/network/FileInfoChannelPacket.java
+++ src/main/java/io/github/kituin/chatimage/network/FileInfoChannelPacket.java
@@ -10,7 +10,7 @@ import net.minecraft.util.Identifier;
 public record FileInfoChannelPacket(String message) implements CustomPayload {
 
     public static final CustomPayload.Id<FileInfoChannelPacket> ID =
-            new CustomPayload.Id<>(new Identifier("chatimage", "file_info"));
+            new CustomPayload.Id<>(Identifier.of("chatimage", "file_info"));
     public static final PacketCodec<PacketByteBuf, FileInfoChannelPacket> CODEC =
             PacketCodec.tuple(PacketCodecs.STRING, FileInfoChannelPacket::message, FileInfoChannelPacket::new);
 
