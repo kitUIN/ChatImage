f2d255b6a230f3781f108c1b7a36c1ab27be9955
Nacho <kulujun@gmail.com>
2024-07-11 04:13:57 +0800

    test ci

--- link.ps1
+++ link.ps1
@@ -15,15 +15,17 @@ $prefix = "fabric"
 
 function Create-SymbolicLink {
     param($target, $path, $source)
-    $fullPath = Join-Path -Path $target -ChildPath $path
-    if (Test-Path $fullPath) {
-        Remove-Item $fullPath -Recurse
-    }
-    if ($ci) {
-        Copy-Item $source -Destination $fullPath -Force -Recurse
-    }
-    else{
-        New-Item -ItemType SymbolicLink -Path $fullPath -Target $source
+    if (Test-Path $source) {
+        $fullPath = Join-Path -Path $target -ChildPath $path
+        if (Test-Path $fullPath) {
+            Remove-Item $fullPath -Recurse
+        }
+        if ($ci) {
+            Copy-Item $source -Destination $fullPath -Force -Recurse
+        }
+        else{
+            New-Item -ItemType SymbolicLink -Path $fullPath -Target $source
+        }
     }
 }
 
