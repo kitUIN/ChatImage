7c5b78fb4035838fa2e0fccf19d214e9c12220dd
kitUIN <kulujun@gmail.com>
2024-11-27 15:13:30 +0800

    :bug: fix

---
+++ neoforge/origin/src/main/java/io/github/kituin/chatimage/network/FileInfoChannelHandler.java
@@ -0,0 +1,44 @@
+// ONLY > neoforge-1.20.3
+package io.github.kituin.chatimage.network;
+
+import com.google.common.collect.Lists;
+import io.github.kituin.ChatImageCode.ChatImageFrame;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.neoforge.network.handling.PlayPayloadContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.github.kituin.ChatImageCode.ClientStorage.AddImageError;
+import static io.github.kituin.ChatImageCode.ServerStorage.*;
+import static io.github.kituin.chatimage.network.ChatImagePacket.loadFromServer;
+import static io.github.kituin.chatimage.network.ChatImagePacket.serverFileInfoChannelReceived;
+
+public class FileInfoChannelHandler {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final FileInfoChannelHandler INSTANCE = new FileInfoChannelHandler();
+
+    public static FileInfoChannelHandler getInstance() {
+        return INSTANCE;
+    }
+
+    public void serverHandle(final FileInfoChannelPacket packet, final PlayPayloadContext ctx) {
+        ctx.player().ifPresent(player -> serverFileInfoChannelReceived((ServerPlayer) player, packet.message()));
+    }
+    public void clientHandle(final FileInfoChannelPacket packet, final PlayPayloadContext ctx) {
+        String data = packet.message();
+        String url = data.substring(6);
+        LOGGER.info(url);
+        if (data.startsWith("null")) {
+            LOGGER.info("[GetFileChannel-NULL]" + url);
+            AddImageError(url, ChatImageFrame.FrameError.FILE_NOT_FOUND);
+        } else if (data.startsWith("true")) {
+            LOGGER.info("[GetFileChannel-Retry]" + url);
+            loadFromServer(url);
+        }
+    }
+}
+
