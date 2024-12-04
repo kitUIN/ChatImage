ef6e81d9a87f6f7f375a148c45097b986aec7be5
kituin <kit_uin@outlook.com>
2024-12-04 20:12:53 +0800

    :construction: CI

--- matrix.sh
+++ matrix.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
-# 获取传入的路径参数
-paths=("$@")
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -path)
+      IFS=',' read -r -a paths <<< "$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 
-# 初始化一个空数组，用于存储所有文件夹对象
-all_folder_objects=()
+# 用于存储所有文件夹的数组
+allFolderObjects=()
 
 # 遍历每个路径
 for path in "${paths[@]}"; do
-    # 查找目录，排除 "origin" 文件夹
-    folders=$(find "$path" -mindepth 1 -maxdepth 1 -type d ! -name "origin")
+  # 获取指定路径下的文件夹
+  folders=$(find "$path" -mindepth 1 -maxdepth 1 -type d)
 
-    # 遍历每个文件夹，生成相应的 JSON 对象
-    for folder in $folders; do
-        mc_version=$(basename "$folder" | sed "s/^$path-//")
-        mc_loader="$path"
-
-        folder_object=$(jq -n \
-            --arg mc_version "$mc_version" \
-            --arg mc_loader "$mc_loader" \
-            '{ "mc-version": $mc_version, "mc-loader": $mc_loader }')
-
-        all_folder_objects+=("$folder_object")
-    done
+  # 过滤掉名为 "origin" 的文件夹
+  for folder in $folders; do
+    folderName=$(basename "$folder")
+    if [[ "$folderName" != "origin" ]]; then
+      # 提取 mc-version 和 mc-loader 信息
+      mcVersion="${folderName//$path-/}"
+      mcLoader="$path"
+      allFolderObjects+=("{\"mc-version\": \"$mcVersion\", \"mc-loader\": \"$mcLoader\"}")
+    fi
+  done
 done
 
-# 使用 jq 创建 JSON 结构
-json=$(jq -n --argjson config "$(echo "[${all_folder_objects[*]}]" | jq -s .)" '{ config: $config }')
+# 创建 JSON 格式的输出
+json=$(printf "[%s]" "$(IFS=,; echo "${allFolderObjects[*]}")")
 
-# 输出结果
+# 输出最终的 JSON 结果
 echo "::set-output name=matrix::$json"
