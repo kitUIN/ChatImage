fe3e4fc7b70ae79249f953b079540888e9484575
kitUIN <kulujun@gmail.com>
2024-12-05 10:15:37 +0800

    :construction: CI

--- matrix.ps1
+++ matrix.ps1
@@ -7,11 +7,29 @@ $allFolderObjects = @()
 foreach ($path in $paths) {
     $folders = Get-ChildItem -Path $path -Directory
     $filteredFolders = $folders | Where-Object { $_.Name -ne "origin" }
+    $supportVersionFile = "$mcLoader\$mcLoader-$mcVersion\support_version.txt"
+
+    # 读取 support_version.txt 文件内容
+    if (Test-Path $supportVersionFile) {
+        $supportVersion = Get-Content -Path $supportVersionFile | Out-String
+        $supportVersion = $supportVersion.Trim()
+    } else {
+        $supportVersion = $mcVersion
+    }
+
+    # 根据 mcLoader 设置 publishLoaders
+    if ($mcLoader -eq "fabric") {
+        $publishLoaders = "fabric quilt"
+    } else {
+        $publishLoaders = $mcLoader
+    }
 
     $folderObjects = $filteredFolders | ForEach-Object {
         [PSCustomObject]@{
             "mc-version" = $_.Name.Replace("$path-", "")
             "mc-loader"  = $path
+            "publish-loaders" = $publishLoaders
+            "publish-version" = $supportVersion
         }
     }
 
