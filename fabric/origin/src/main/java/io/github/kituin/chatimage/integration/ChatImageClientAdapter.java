3ebbafb6078fd29acb97b709964e7b9198f02757
kitUIN <kulujun@gmail.com>
2024-12-13 20:25:23 +0800

    :bug: support 1.21.4-fabric

--- fabric/origin/src/main/java/io/github/kituin/chatimage/integration/ChatImageClientAdapter.java
+++ fabric/origin/src/main/java/io/github/kituin/chatimage/integration/ChatImageClientAdapter.java
@@ -1,17 +1,20 @@
 package io.github.kituin.chatimage.integration;
 
+import com.google.common.collect.Maps;
 import io.github.kituin.ChatImageCode.ChatImageFrame;
 import io.github.kituin.ChatImageCode.IClientAdapter;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.texture.NativeImageBackedTexture;
-import net.minecraft.text.Text;
+import #Component#;
 import net.minecraft.util.Identifier;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 // IF >= fabric-1.20.5
 //import io.github.kituin.chatimage.network.FileChannelPacket;
 // END IF
@@ -22,6 +25,23 @@ import static io.github.kituin.chatimage.network.ChatImagePacket.*;
 import static io.github.kituin.chatimage.tool.SimpleUtil.createTranslatableComponent;
 
 public class ChatImageClientAdapter implements IClientAdapter {
+// IF >= fabric-1.21.4
+    private final Map<String, Integer> dynamicIdCounters = Maps.newHashMap();
+    public Identifier registerDynamicTexture(String prefix, NativeImageBackedTexture texture) {
+        Integer integer = (Integer)this.dynamicIdCounters.get(prefix);
+        if (integer == null) {
+            integer = 1;
+        } else {
+            integer = integer + 1;
+        }
+
+        this.dynamicIdCounters.put(prefix, integer);
+        Identifier identifier = Identifier.ofVanilla(String.format(Locale.ROOT, "dynamic/%s_%d", prefix, integer));
+        MinecraftClient.getInstance().getTextureManager().registerTexture(identifier, texture);
+        return identifier;
+    }
+// END IF
+
     @Override
     public int getTimeOut() {
         return CONFIG.timeout;
@@ -31,7 +51,12 @@ public class ChatImageClientAdapter implements IClientAdapter {
     public ChatImageFrame.TextureReader<Identifier> loadTexture(InputStream image) throws IOException {
         NativeImage nativeImage = NativeImage.read(image);
         return new ChatImageFrame.TextureReader<>(
-                MinecraftClient.getInstance().getTextureManager().registerDynamicTexture(MOD_ID + "/chatimage",
+// IF >= fabric-1.21.4
+                registerDynamicTexture(
+// ELSE
+////                         MinecraftClient.getInstance().getTextureManager().registerDynamicTexture(
+// END IF
+                        MOD_ID + "/chatimage",
                         new NativeImageBackedTexture(nativeImage)),
                 nativeImage.getWidth(),
                 nativeImage.getHeight()
