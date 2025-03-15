f51c4d64143b6ace4c64cfb9ec61da0f909c1254
kituin <kit_uin@outlook.com>
2025-03-16 01:36:07 +0800

    :bug: Fix Forge-1.16.5 No KeyBindings

--- forge/origin/src/main/java/io/github/kituin/chatimage/KeyBindings.java
+++ forge/origin/src/main/java/io/github/kituin/chatimage/KeyBindings.java
@@ -1,45 +1,23 @@
 package io.github.kituin.chatimage;
 
-
-// IF forge-1.16.5
-//import net.minecraft.client.settings.KeyBinding;
-// ELSE
-//import net.minecraft.client.KeyMapping;
-//import com.mojang.blaze3d.platform.InputConstants;
-// END IF
-// IF forge-1.16.5
-//import net.minecraftforge.fml.client.registry.ClientRegistry;
-// ELSE IF forge-1.18.2
-//import net.minecraftforge.client.ClientRegistry;
-// ELSE
-//import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-// END IF
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
-// IF forge-1.16.5
-//    public static KeyBinding gatherManaKeyMapping;
-// ELSE
-//    public static KeyMapping gatherManaKeyMapping;
-// END IF
-
+    public static #KeyMapping# gatherManaKeyMapping;
 
     public static void init(
 // IF > forge-1.18.2
-//            RegisterKeyMappingsEvent event
+//            net.minecraftforge.client.event.RegisterKeyMappingsEvent event
 // END IF
     ) {
-// IF forge-1.16.5
-// ELSE
-//        gatherManaKeyMapping = new KeyMapping(
-//                "config.chatimage.key",
-//                InputConstants.Type.KEYSYM,
-//                GLFW.GLFW_KEY_END,
-//                "config.chatimage.category");
-// END IF
+       gatherManaKeyMapping = new #KeyMapping#(
+               "config.chatimage.key",
+               #InputConstants#.Type.KEYSYM,
+               GLFW.GLFW_KEY_END,
+               "config.chatimage.category");
 
 // IF < forge-1.19
-//        ClientRegistry.registerKeyBinding(gatherManaKeyMapping);
+//        #ClientRegistry#.registerKeyBinding(gatherManaKeyMapping);
 // ELSE
 //        event.register(gatherManaKeyMapping);
 // END IF
