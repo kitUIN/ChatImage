a007b01530c598f12d5d86f5737187536a34962f
kituin <kit_uin@outlook.com>
2025-04-03 01:58:29 +0800

    :bug: fix HoverEvent Action

--- fabric/origin/src/main/java/io/github/kituin/chatimage/mixin/HoverEventActionMixin.java
+++ fabric/origin/src/main/java/io/github/kituin/chatimage/mixin/HoverEventActionMixin.java
@@ -2,13 +2,15 @@
 package io.github.kituin.chatimage.mixin;
 
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import io.github.kituin.chatimage.tool.ChatImageStyle;
 import #HoverEvent#;
+import net.minecraft.util.StringIdentifiable;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,39 +18,41 @@ import java.util.Arrays;
 @Unique
 public abstract class HoverEventActionMixin {
 
-    // @Shadow
-    // @Final
-    // @Mutable
-    // private static HoverEvent.Action[] $VALUES;
-
+    @Shadow
+    @Final
+    @Mutable
+    private static HoverEvent.Action[] field_55910;
     private static final HoverEvent.Action SHOW_IMAGE = chatimage$addVariant("SHOW_IMAGE", "show_chatimage", true, ChatImageStyle.ShowImage.CODEC);
+
+    @Shadow
+    @Final
+    @Mutable
+    public static Codec<HoverEvent.Action> UNVALIDATED_CODEC;
+    @Shadow
+    @Final
+    @Mutable
+    public static Codec<HoverEvent.Action> CODEC;
+
 
     @Invoker("<init>")
     public static HoverEvent.Action chatimage$invokeInit(String internalName, int internalId, String name, boolean parsable, MapCodec<? extends HoverEvent> codec) {
         throw new AssertionError();
     }
 
+    @Unique
     private static HoverEvent.Action chatimage$addVariant(String internalName, String name, boolean parsable, MapCodec<? extends HoverEvent> codec) {
-        try {
-            // 获取枚举类的构造函数和静态字段
-            Field valuesField = HoverEvent.Action.class.getDeclaredField("$VALUES");
-            valuesField.setAccessible(true);  // 设置可访问
+        ArrayList<HoverEvent.Action> variants = new ArrayList<>(Arrays.asList(HoverEventActionMixin.field_55910));
+        HoverEvent.Action newAction = chatimage$invokeInit(internalName, variants.size(), name, parsable, codec);
 
-            HoverEvent.Action[] originalValues = (HoverEvent.Action[]) valuesField.get(null);
+        variants.add(newAction);
+        HoverEventActionMixin.field_55910 = variants.toArray(new HoverEvent.Action[0]);
+        UNVALIDATED_CODEC = StringIdentifiable.createBasicCodec(() -> HoverEventActionMixin.field_55910);
+        CODEC = UNVALIDATED_CODEC.validate(HoverEventActionMixin::invokeValidate);
+        return newAction;
+    }
 
-            // 创建新的 Action
-            ArrayList<HoverEvent.Action> variants = new ArrayList<>(Arrays.asList(originalValues));
-            HoverEvent.Action newAction = chatimage$invokeInit(internalName, variants.size(), name, parsable, codec);
-
-            // 添加到列表并更新 $VALUES
-            variants.add(newAction);
-            valuesField.set(null, variants.toArray(new HoverEvent.Action[0]));
-
-            return newAction;
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-            return null;
-        }
+    @Invoker("validate")
+    public static DataResult<HoverEvent.Action> invokeValidate(HoverEvent.Action action) {
+        throw new AssertionError();
     }
 }
