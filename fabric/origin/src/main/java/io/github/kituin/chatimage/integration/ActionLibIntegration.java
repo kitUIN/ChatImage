2ddfde89db06b21e416b68e7ea19f14ce3a72952
kituin <kit_uin@outlook.com>
2025-04-03 16:48:01 +0800

    :sparkles: support hoverevent codec

--- fabric/origin/src/main/java/io/github/kituin/chatimage/integration/ActionLibIntegration.java
+++ fabric/origin/src/main/java/io/github/kituin/chatimage/integration/ActionLibIntegration.java
@@ -1,16 +1,21 @@
-// ONLY < fabric-1.21.5
 package io.github.kituin.chatimage.integration;
 
 import io.github.kituin.actionlib.IActionRegisterApi;
-import #HoverEvent#;
+import io.github.kituin.chatimage.tool.ChatImageStyle;
 
 import java.util.List;
 
-import static io.github.kituin.chatimage.tool.ChatImageStyle.SHOW_IMAGE;
 
 public class ActionLibIntegration implements IActionRegisterApi {
-    @Override
-    public List<HoverEvent.Action> registerHoverEventAction() {
-        return List.of(SHOW_IMAGE);
-    }
+// IF >= fabric-1.21.5
+//    @Override
+//    public List<io.github.kituin.actionlib.HoverEventActionVariant> registerHoverEventAction() {
+//        return List.of(new io.github.kituin.actionlib.HoverEventActionVariant("SHOW_IMAGE", "show_chatimage", true, ChatImageStyle.ShowImage.CODEC));
+//    }
+// ELSE
+//    @Override
+//    public List<#HoverEvent#.Action> registerHoverEventAction() {
+//        return List.of(ChatImageStyle.SHOW_IMAGE);
+//    }
+// END IF
 }
